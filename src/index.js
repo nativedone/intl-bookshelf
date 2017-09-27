97a96fafbbe95ad106fd376c7ced68343858b061
Adeildo Amorim <adeildoamorim@gmail.com>
2017-09-27 10:45:39 +1000

    Add react-intl and include the basic configuration to allow translation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { addLocaleData, IntlProvider } from 'react-intl'
+
+// Languages to be used
+import en from 'react-intl/locale-data/en'
+import es from 'react-intl/locale-data/es'
+import fr from 'react-intl/locale-data/fr'
+
+import messages from './messages'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Instantiate the addLocaleData
+addLocaleData([...en, ...es, ...fr])
+
+// Get the use's locale
+const fallbackLanguage = 'en-US'
+const preferedUserLanguage = navigator.languages[0]
+let locale = 
+    (navigator.languages && preferedUserLanguage)
+    || navigator.language // if it doesn't support navigator.languages
+    || navigator.userLanguage // usually internet explorer
+    || fallbackLanguage
+
+ReactDOM.render(
+    <IntlProvider locale={locale} messages={messages[locale]}>
+        <App />
+    </IntlProvider>, 
+    document.getElementById('root')
+);
+
 registerServiceWorker();
